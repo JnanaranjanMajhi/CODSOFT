fe9ba7e2b345d3e4c97762c5d4fd690a53597af3
JNANARANJAN MAJHI <137917191+JnanaranjanMajhi@users.noreply.github.com>
2024-11-20 20:32:19 +0530

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,17 +1,13 @@
 def add(x, y):
     return x + y
-
 def subtract(x, y):
     return x - y
-
 def multiply(x, y):
     return x * y
-
 def divide(x, y):
     if y == 0:
         return "Division by zero is not possible."
     return x / y
-
 def calculator():
     print("Select operation:")
     print("1. Addition")
@@ -40,6 +36,5 @@ def calculator():
                 break
         else:
             print("Invalid choice! Please select a valid operation.")
-
 if __name__ == "__main__":
     calculator()
